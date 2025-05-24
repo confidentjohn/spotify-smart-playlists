fc4a5c364eb49f3247cc323aebdfa9b26dc157ba
Mitchell <jmitchel@elc-ct4tlqfw15.lan>
2025-05-24 12:35:52 -0400

    Fix: use refresh token for Spotify server auth

--- track_plays.py
+++ track_plays.py
@@ -1,18 +1,26 @@
 import os
 import psycopg2
+import requests
 from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime
 from zoneinfo import ZoneInfo  # Python 3.9+
 
-# Spotify authentication
-sp = Spotify(auth_manager=SpotifyOAuth(
-    client_id=os.environ['SPOTIFY_CLIENT_ID'],
-    client_secret=os.environ['SPOTIFY_CLIENT_SECRET'],
-    redirect_uri=os.environ['SPOTIFY_REDIRECT_URI'],
-    scope='user-read-recently-played user-library-read',
-    cache_path=None
-))
+# Function to get a new access token using the refresh token
+def get_access_token():
+    auth_response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data={
+            'grant_type': 'refresh_token',
+            'refresh_token': os.environ['SPOTIFY_REFRESH_TOKEN'],
+            'client_id': os.environ['SPOTIFY_CLIENT_ID'],
+            'client_secret': os.environ['SPOTIFY_CLIENT_SECRET']
+        }
+    )
+    return auth_response.json()['access_token']
+
+# Get the access token and init Spotipy client
+access_token = get_access_token()
+sp = Spotify(auth=access_token)
 
 # Connect to PostgreSQL
 conn = psycopg2.connect(
@@ -20,7 +28,7 @@ conn = psycopg2.connect(
     user=os.environ['DB_USER'],
     password=os.environ['DB_PASSWORD'],
     host=os.environ['DB_HOST'],
-    port=os.environ.get('DB_PORT', 5432)
+    port=os.environ.get('DB_PORT', 5432),
 )
 cur = conn.cursor()
 
@@ -36,17 +44,17 @@ for item in results['items']:
     played_at = item['played_at']
     played_at = datetime.fromisoformat(played_at.replace('Z', '+00:00')).astimezone(ZoneInfo('UTC'))
 
-    # Check if track is in user's saved library
+    # Check if track is saved in your library
     is_liked = sp.current_user_saved_tracks_contains([track_id])[0]
 
-    # Insert or update track
+    # Insert or update track metadata
     cur.execute("""
         INSERT INTO tracks (id, name, artist, album, is_liked)
         VALUES (%s, %s, %s, %s, %s)
         ON CONFLICT (id) DO UPDATE SET is_liked = EXCLUDED.is_liked;
     """, (track_id, name, artist, album, is_liked))
 
-    # Insert play if not already logged
+    # Insert play history (skip if already exists)
     cur.execute("""
         INSERT INTO plays (track_id, played_at)
         VALUES (%s, %s)
