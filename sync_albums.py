fa6bb40e5827e15fc3f296980951b159ad7a0faa
Mitchell <jmitchel@elc-ct4tlqfw15.lan>
2025-05-24 14:50:57 -0400

    Add sync_albums script to pull saved albums and their tracks

--- sync_albums.py
+++ sync_albums.py
@@ -45,11 +45,26 @@ while True:
         release_date = album.get('release_date')
         total_tracks = album.get('total_tracks')
 
+        # Insert album record
         cur.execute("""
             INSERT INTO albums (id, name, artist, release_date, total_tracks)
             VALUES (%s, %s, %s, %s, %s)
             ON CONFLICT (id) DO NOTHING;
         """, (album_id, name, artist, release_date, total_tracks))
+
+        # Insert each track in the album into the tracks table
+        album_tracks = sp.album(album_id)['tracks']['items']
+        for track in album_tracks:
+            track_id = track['id']
+            track_name = track['name']
+            track_artist = track['artists'][0]['name']
+            track_album = album['name']
+
+            cur.execute("""
+                INSERT INTO tracks (id, name, artist, album, is_liked, from_album)
+                VALUES (%s, %s, %s, %s, FALSE, TRUE)
+                ON CONFLICT (id) DO UPDATE SET from_album = TRUE;
+            """, (track_id, track_name, track_artist, track_album))
 
     offset += len(items)
     if len(items) < limit:
@@ -58,4 +73,4 @@ while True:
 conn.commit()
 cur.close()
 conn.close()
-print("✅ Synced saved albums from library.")
+print("✅ Synced saved albums and their tracks.")
