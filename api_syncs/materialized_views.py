4e47d65fd9d6c909626de478177daae42db40ee8
John Mitchell <johnnydmitch@gmail.com>
2025-06-02 10:53:33 -0400

    hijf

--- api_syncs/materialized_views.py
+++ api_syncs/materialized_views.py
@@ -4,65 +4,62 @@ from utils.logger import log_event
 
 UNIFIED_TRACKS_VIEW = """
 CREATE MATERIALIZED VIEW IF NOT EXISTS unified_tracks AS
-WITH track_data AS (
-    -- Tracks from albums (with enriched metadata)
-    SELECT 
-        t.id AS track_id,
-        t.name AS track_name,
-        COALESCE(a.artist, lt.track_artist) AS artist,
-        t.album_id,
-        a.name AS album_name,
-        a.release_date,
-        t.track_number,
-        t.added_at,
-        COALESCE(lt.liked_at, t.date_liked_at) AS liked_at,
-        COALESCE(lt.last_checked_at, t.date_liked_checked) AS last_checked_at,
-        ta.is_playable,
-        COUNT(p.played_at) AS play_count,
-        MIN(p.played_at) AS first_played_at,
-        MAX(p.played_at) AS last_played_at,
-        CASE 
-            WHEN COALESCE(lt.liked_at, t.date_liked_at) IS NOT NULL THEN TRUE
-            ELSE FALSE
-        END AS is_liked
-    FROM tracks t
-    JOIN albums a ON t.album_id = a.id
-    LEFT JOIN liked_tracks lt ON lt.track_id = t.id
-    LEFT JOIN track_availability ta ON ta.track_id = t.id
-    LEFT JOIN plays p ON p.track_id = t.id
-    WHERE a.is_saved = TRUE
-    GROUP BY 
-        t.id, t.name, a.artist, lt.track_artist, t.album_id, a.name, a.release_date,
-        t.track_number, t.added_at, lt.liked_at, lt.last_checked_at, ta.is_playable,
-        t.date_liked_at, t.date_liked_checked
+-- Tracks from albums (with enriched metadata)
+SELECT 
+    t.id AS track_id,
+    t.name AS track_name,
+    COALESCE(a.artist, lt.track_artist) AS artist,
+    t.album_id,
+    a.name AS album_name,
+    a.release_date,
+    t.track_number,
+    t.added_at,
+    COALESCE(lt.liked_at, t.date_liked_at) AS liked_at,
+    COALESCE(lt.last_checked_at, t.date_liked_checked) AS last_checked_at,
+    ta.is_playable,
+    COUNT(p.played_at) AS play_count,
+    MIN(p.played_at) AS first_played_at,
+    MAX(p.played_at) AS last_played_at,
+    CASE 
+        WHEN COALESCE(lt.liked_at, t.date_liked_at) IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END AS is_liked
+FROM tracks t
+JOIN albums a ON t.album_id = a.id
+LEFT JOIN liked_tracks lt ON lt.track_id = t.id
+LEFT JOIN track_availability ta ON ta.track_id = t.id
+LEFT JOIN plays p ON p.track_id = t.id
+WHERE a.is_saved = TRUE
+GROUP BY 
+    t.id, t.name, a.artist, lt.track_artist, t.album_id, a.name, a.release_date,
+    t.track_number, t.added_at, lt.liked_at, lt.last_checked_at, ta.is_playable,
+    t.date_liked_at, t.date_liked_checked
 
-    UNION ALL
+UNION ALL
 
-    -- Liked tracks not in album-based `tracks` table
-    SELECT 
-        lt.track_id,
-        lt.track_name,
-        lt.track_artist AS artist,
-        NULL AS album_id,
-        NULL AS album_name,
-        NULL AS release_date,
-        NULL AS track_number,
-        lt.added_at,
-        lt.liked_at,
-        lt.last_checked_at,
-        ta.is_playable,
-        COUNT(p.played_at) AS play_count,
-        MIN(p.played_at) AS first_played_at,
-        MAX(p.played_at) AS last_played_at,
-        TRUE AS is_liked
-    FROM liked_tracks lt
-    LEFT JOIN tracks t ON lt.track_id = t.id
-    LEFT JOIN track_availability ta ON ta.track_id = lt.track_id
-    LEFT JOIN plays p ON p.track_id = lt.track_id
-    WHERE t.id IS NULL
-    GROUP BY lt.track_id, lt.track_name, lt.track_artist, lt.added_at, lt.liked_at, lt.last_checked_at, ta.is_playable
-)
-SELECT * FROM track_data;
+-- Liked tracks not in album-based `tracks` table
+SELECT 
+    lt.track_id,
+    lt.track_name,
+    lt.track_artist AS artist,
+    NULL AS album_id,
+    NULL AS album_name,
+    NULL AS release_date,
+    NULL AS track_number,
+    lt.added_at,
+    lt.liked_at,
+    lt.last_checked_at,
+    ta.is_playable,
+    COUNT(p.played_at) AS play_count,
+    MIN(p.played_at) AS first_played_at,
+    MAX(p.played_at) AS last_played_at,
+    TRUE AS is_liked
+FROM liked_tracks lt
+LEFT JOIN tracks t ON lt.track_id = t.id
+LEFT JOIN track_availability ta ON ta.track_id = lt.track_id
+LEFT JOIN plays p ON p.track_id = lt.track_id
+WHERE t.id IS NULL
+GROUP BY lt.track_id, lt.track_name, lt.track_artist, lt.added_at, lt.liked_at, lt.last_checked_at, ta.is_playable;
 """
 
 if __name__ == "__main__":
