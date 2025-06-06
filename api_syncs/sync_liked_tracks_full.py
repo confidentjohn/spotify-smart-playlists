cc235da7cfb7fa3f5f17c149363e97dd9657db7f
John Mitchell <johnnydmitch@gmail.com>
2025-06-06 13:54:24 -0400

    another fix

--- api_syncs/sync_liked_tracks_full.py
+++ api_syncs/sync_liked_tracks_full.py
@@ -111,17 +111,20 @@ with open(LOCK_FILE, 'w') as lock_file:
 
             # Insert into liked_tracks table
             cur.execute("""
-                INSERT INTO liked_tracks (track_id, liked_at, added_at, last_checked_at, track_name, track_artist, album_id, album_in_library)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                ON CONFLICT (track_id) DO UPDATE 
-                SET liked_at = EXCLUDED.liked_at,
-                    added_at = EXCLUDED.added_at,
-                    last_checked_at = EXCLUDED.last_checked_at,
-                    track_name = EXCLUDED.track_name,
-                    track_artist = EXCLUDED.track_artist,
-                    album_id = EXCLUDED.album_id,
-                    album_in_library = EXCLUDED.album_in_library;
-            """, (track_id, liked_added_at, final_added_at, now, name, artist, album_id, album_in_library))
+            INSERT INTO liked_tracks (
+                track_id, liked_at, added_at, last_checked_at,
+                track_name, track_artist, album_in_library, album_id
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            ON CONFLICT (track_id) DO UPDATE 
+            SET liked_at = EXCLUDED.liked_at,
+                added_at = EXCLUDED.added_at,
+                last_checked_at = EXCLUDED.last_checked_at,
+                track_name = EXCLUDED.track_name,
+                track_artist = EXCLUDED.track_artist,
+                album_in_library = EXCLUDED.album_in_library,
+                album_id = EXCLUDED.album_id;
+            """, (track_id, liked_added_at, final_added_at, now, name, artist, album_in_library, album_id))
 
             updated_liked_tracks += 1
             counter += 1
