48282519222902304f567881474361fccec6c0c6
John Mitchell <johnnydmitch@gmail.com>
2025-06-22 22:56:35 -0400

    deply

--- app/startup.py
+++ app/startup.py
@@ -1,8 +1,10 @@
 from app.db.init_db import run_init_db
 from utils.create_exclusions_playlist import ensure_exclusions_playlist
+from utils.spotify_auth import get_spotify_client
 
 def run_startup_tasks():
     run_init_db()
-    ensure_exclusions_playlist()
+    sp = get_spotify_client()
+    ensure_exclusions_playlist(sp)
 
 run_startup_tasks()
